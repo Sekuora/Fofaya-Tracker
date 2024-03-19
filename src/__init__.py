8048c93b2bc22624e67439818f1d8f4b7abf358e
sekuora <162371601+Sekuora@users.noreply.github.com>
2024-03-19 18:26:05 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,6 @@
 # This file is part of a software tool distributed under the GNU General Public License.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 from .time_track_data import TimeTrackData
 from .main_window_ui import MainWindow
 from .time_tracker import TimeTracker
